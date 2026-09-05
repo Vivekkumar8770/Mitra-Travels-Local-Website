bf4aafd219a8dd9474a572c87de629d58b0bd282
vivek <rathorevivek050@gmail.com>
2026-09-05 19:59:56 +0530

    h

--- components/package-browser.tsx
+++ components/package-browser.tsx
@@ -13,8 +13,8 @@ export function PackageBrowser({ items }: { items: TourPackage[] }) {
           <TabsTrigger value="nepal" className="package-tab rounded-lg px-5 py-3 text-sm data-[state=active]:bg-[#0a2a50] data-[state=active]:text-white"><Mountain className="size-4" />Nepal packages<span className="package-tab-caption">Himalayan journeys</span></TabsTrigger>
           <TabsTrigger value="india" className="package-tab rounded-lg px-5 py-3 text-sm data-[state=active]:bg-[#0a2a50] data-[state=active]:text-white"><Landmark className="size-4" />India packages<span className="package-tab-caption">Heritage & pilgrimage</span></TabsTrigger>
       </TabsList>
-      <TabsContent value="nepal"><div className="mt-8 grid gap-6 md:grid-cols-2 xl:grid-cols-3">{all.filter((item) => item.country === "Nepal").map((item) => <PackageCard key={item.slug} item={item} />)}</div></TabsContent>
-      <TabsContent value="india"><div className="mt-8 grid gap-6 md:grid-cols-2 xl:grid-cols-3">{all.filter((item) => item.country === "India").map((item) => <PackageCard key={item.slug} item={item} />)}</div></TabsContent>
+      <TabsContent value="nepal"><div className="package-browser-grid mt-8 grid gap-6 md:grid-cols-2 xl:grid-cols-3">{all.filter((item) => item.country === "Nepal").map((item) => <PackageCard key={item.slug} item={item} />)}</div></TabsContent>
+      <TabsContent value="india"><div className="package-browser-grid mt-8 grid gap-6 md:grid-cols-2 xl:grid-cols-3">{all.filter((item) => item.country === "India").map((item) => <PackageCard key={item.slug} item={item} />)}</div></TabsContent>
     </Tabs>
   );
 }
